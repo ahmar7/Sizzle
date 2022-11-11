a8f322d3e6846d24acbbe216ba5c306524ba2891
A. Developers <ahmarjabbar7@gmail.com>
2022-11-11 19:26:26 +0500

    new

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -6,9 +6,7 @@ const Navbar = () => {
     <>
       {" "}
       <div className="navbar">
-        <div className="left-bar">
-          <img src={Logo} alt="" />
-        </div>
+        <div className="left-bar">{/* <img src={Logo} alt="" /> */}</div>
         <div className="right-bar">
           <button>Apply</button>
         </div>
