3ef0580060c64ce13082a14d0f6d7376ba41bbdd
A. Developers <ahmarjabbar7@gmail.com>
2022-11-15 02:51:06 +0500

    tech update

--- src/components/Tech/tech.js
+++ src/components/Tech/tech.js
@@ -4,41 +4,49 @@ import "./tech.css";
 const Tech = () => {
   return (
     <div className="tech-section">
-      <video
-        className="animation"
-        loop={true}
-        autoPlay={true}
-        muted={true}
-        src={Animate}
-      ></video>
-      <p className="heading-tech">ADVANCED - TECH </p>
-      <h1 className="tech-h">
-        OUR PLATFORM MADE <br /> SIMPLE & EASY FOR YOU
-      </h1>
-      <p className="tech-para">
-        WE CREATED THE TECHNOLOGY TO ALLOW YOU TO CONCENTRATE ON WHAT YOU DO
-        BEST: COOKING AMAZING FOOD & PROVIDING AMAZING SERVICE. OUR PLATFORM
-        MAKES EVERYTHING EASY & EFFICIENT, SO YOU CAN CONTINUE TO RAISE YOUR
-        MARGINS.
-      </p>
-      <div className="tech-card">
-        <div className="indiv-tech">
-          <img src={Tech1} alt="" />
-          <h3 className="indiv-h">RESULT - ORIENTED</h3>
-          <p className="indiv-p">
-            KEEP TRACK OF YOUR RESTAURANT INCLUDING PROFIT, REVENUE & MORE. ALSO
-            GET EASY ACCESS TO LAUNCH MORE
-            <span className="orange"> SIZZLE</span> BRANDS
+      <div className="left-tech">
+        <p className="heading-tech">ADVANCED - TECH </p>
+        <h1 className="tech-h">
+          OUR PLATFORM MADE <br /> SIMPLE & EASY FOR YOU
+        </h1>
+        <p className="tech-para">
+          WE CREATED THE TECHNOLOGY TO ALLOW YOU TO CONCENTRATE ON WHAT YOU DO
+          BEST: COOKING AMAZING FOOD & PROVIDING AMAZING SERVICE. OUR PLATFORM
+          MAKES EVERYTHING EASY & EFFICIENT, SO YOU CAN CONTINUE TO RAISE YOUR
+          MARGINS.
+        </p>
+      </div>
+      <div className="right-tech">
+        <div className="left-inner no-mar">
+          <h3>RESULT - ORIENTED</h3>
+          <p>
+            KEEP TRACK OF <span className="orange">YOUR RESTAURANT </span>{" "}
+            INCLUDING PROFIT, REVENUE & MORE. ALSO GET EASY ACCESS TO LAUNCH
+            MORE SIZZLE BRANDS
           </p>
         </div>
-        <div className="indiv-tech">
-          <img className="sec-img" src={Tech2} alt="" />
-          <h3 className="indiv-h">EASY TO MANAGE</h3>
-          <p className="indiv-p">
+        <div className="left-inner">
+          <h3>TRAINING PROGRAM</h3>
+          <p>
+            OUR STATE-OF-THE-ART TRAINING PROGRAM WILL PROVIDE YOU &
+            <span className="orange">YOUR STAFF</span>A SEAMLESS EXPERIENCE WHEN
+            LEARNING HOW TO COOK OUR RECIPES
+          </p>
+        </div>
+        <div className="left-inner ">
+          <h3>PAYOUT SYSTEM</h3>
+          <p>
+            AN AUTOMATED SYSTEM THAT ALLOWS YOU TO NOT WORRY ABOUT
+            <span className="orange">YOUR CASH FLOW</span>. WE ARE JUST HERE TO
+            INCREASE YOUR TOP LINE
+          </p>
+        </div>
+        <div className="left-inner">
+          <h3>EASY TO MANAGE</h3>
+          <p>
             OUR SOPHISTICATED YET SIMPLE INTERFACE GIVES YOU THE ABILITY TO
-            MANAGE
-            <span className="orange"> YOUR VIRTUAL BRAND </span> RIGHT FROM YOUR
-            PHONE OR COMPUTER. EVERYTHING YOU NEED IN ONE PLACE.
+            MANAGE <span className="orange">YOUR VIRTUAL BRAND</span> RIGHT FROM
+            YOUR TABLET THAT WE PROVIDE TO YOU. EVERYTHING YOU NEED IN ONE PLACE
           </p>
         </div>
       </div>
